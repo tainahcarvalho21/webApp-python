43d92928fa2e23a72cb296403d97807cf57553db
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-07-05 21:42:11 -0300

    primeiras configurações do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webApp-python
+Repositório de estudos. 
+Se trata de um site que exibe uma lista de jogos e suas respectivas informações. Além disso, vai permitir também cadastrar novos jogos, fazer login e restringir acesso.
---
+++ jogoteca.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def ola():
+    lista = ['Tetris', 'Skyrim', 'Crash'] 
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+app.run()
