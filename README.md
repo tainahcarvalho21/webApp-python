4d93e0b1a8d336dd08289d2e903bfe26efcd7f16
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-07-05 21:44:42 -0300

    primeira parte do app

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # webApp-python
-Repositório de estudos. 
-Se trata de um site que exibe uma lista de jogos e suas respectivas informações. Além disso, vai permitir também cadastrar novos jogos, fazer login e restringir acesso.
+Repositório de estudos.
+ 
+O projeto consiste em uma aplicação web desenvolvida usando Flask, onde os usuários podem realizar autenticação, explorar uma lista de jogos e interagir com recursos como formulários e URLs dinâmicas. Durante o curso, foram abordados os seguintes tópicos principais:
+
+- Configuração e estrutura do projeto Flask
+- Roteamento de URLs usando a função **`@app.route`**
+- Integração de Python com HTML usando a função **`render_template`** do Flask
+- Utilização do paradigma de orientação a objetos no projeto
+- Processamento de formulários e manipulação de métodos POST e GET
+- Depuração de código usando o recurso de debugger do Flask
+- Utilização do framework Bootstrap para estilização do site
+- Utilização de herança no HTML para reutilização de código
+- Utilização da função **`url_for`** para criar URLs de forma dinâmica
+- Armazenamento de dados usando o recurso de sessão do Flask
+- Passagem de informações entre páginas usando query string
+- Desenvolvimento de um projeto completo, aplicando os conceitos aprendidos
--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,78 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+
+usuario1 = Usuario("Tainah", "Tai", "alohomora")
+usuario2 = Usuario("Camila Ferreira,", "Mila", "paozinho")
+usuario3 = Usuario("Guilherme Ferreira", "Cake", "python_eh_vida")
+usuarios = { usuario1.nickname : usuario1,
+             usuario2.nickname : usuario2,
+             usuario3.nickname : usuario3 }
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alohomora'
 
-@app.route('/inicio')
-def ola():
-    lista = ['Tetris', 'Skyrim', 'Crash'] 
+@app.route('/')
+def index(): 
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Novo Jogo')
 
-app.run()
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST'],)
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            if proxima_pagina == "None":
+                proxima_pagina = url_for('index')
+            return redirect(proxima_pagina)
+        else:
+            flash('Senha inválida!')
+            return redirect(url_for('login'))
+    else:
+        flash('Usuário não logado')
+        return redirect(url_for('login'))
+    
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
+
+app.run(port=3000, debug=True)
